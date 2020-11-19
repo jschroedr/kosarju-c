334f4e9fe9e6261e311097e5c63d5f5942b7859a
jwschroeder330 <jschroeder@linkmedia360.com>
2020-11-18 20:08:24 -0500

    testing

--- kosarju-c/main.c
+++ kosarju-c/main.c
@@ -21,7 +21,7 @@
 
 
 char * getExampleFileName() {
-   return "./test-files/input_mostlyCycles_1_8.txt";
+   return "./test-files/input_mostlyCycles_68_320000.txt";
 }
 
 /*
@@ -33,30 +33,10 @@ int main(int argc, char** argv) {
     int nrows = 0;
     char ** rows = loadFromAdjacencyList(fname, &nrows);
     graph * g = loadd_adj_list(rows, &nrows);
-
-    printf("\n***** Graph After Kosarju *******");
-    kosarju_scc_twopass(g);
-    for(int i = 0; i < g->vlen; i ++) {
-        vertex * leader = g->vertices[i]->leader;
-        printf("\n vertex: %s, scc: %s", g->vertices[i]->label, leader->label);
-    }
-    
+ 
     garray * groups = groups_from_graph(g);
    
-    printf("\n***** Graph Groups Unordered *******");
-    for(int j = 0; j < groups->glen; j ++) {
-        group * gr = groups->groups[j];
-        printf("\n group: %s, glen: %d", gr->leader->label, gr->len);
-    }
-    printf("\n**************************************");
-    
     sort_groups_by_size(groups);
-    
-    printf("\n***** Graph Groups By Size *******");
-    for(int k = 0; k < groups->glen; k ++) {
-        group * gr = groups->groups[k];
-        printf("\n group: %s, glen: %d", gr->leader->label, gr->len);
-    }
     
     printf("\n");
     for(int k = 0; k < 5; k ++) {
